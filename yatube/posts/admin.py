01da681d7c60b09f275ceb5642ce77fe89bbe9f9
maximxmoroz <maximxmoroz@gmail.com>
2021-10-29 20:17:35 +0500

    p

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from .models import Group, Post
 
-
-
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author', 'group')
@@ -13,6 +11,7 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 @admin.register(Group)
+
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
     search_fields = ('title',)
